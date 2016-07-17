162f7f1f20e3add917b2bf16a9cd9eb695c1018f
George Hopkins <george-hopkins@null.net>
2016-07-17 14:06:03 +0200

    Fix option parser in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -9,38 +9,45 @@ srcdir=`dirname $0`
 pushd $srcdir > /dev/null
 gtkdocize && \
 autoreconf --verbose --force --install --make || {
- echo 'autogen.sh failed';
- exit 1;
+  echo "autogen.sh failed"
+  exit 1
 }
 
 popd > /dev/null
 
-while test "x$@" != "x" ; do
-optarg=`expr "x$@" : 'x[^=]*=\(.*\)'`
-case "$@" in
-  --noconfigure)
+while test "x$1" != "x" ; do
+  optarg=`expr "x$1" : 'x[^=]*=\(.*\)'`
+  case "$1" in
+    --noconfigure)
       NOCONFIGURE=defined
-  AUTOGEN_EXT_OPT="$AUTOGEN_EXT_OPT --noconfigure"
+      AUTOGEN_EXT_OPT="$AUTOGEN_EXT_OPT --noconfigure"
       echo "+ configure run disabled"
       shift
       ;;
-esac
+    --)
+      shift
+      break
+      ;;
+    *)
+      CONFIGURE_EXT_OPT="$CONFIGURE_EXT_OPT $1"
+      shift
+      ;;
+  esac
 done
 
 for arg do CONFIGURE_EXT_OPT="$CONFIGURE_EXT_OPT $arg"; done
-if test ! -z "$CONFIGURE_EXT_OPT"
-then
-echo "+ options passed to configure: $CONFIGURE_EXT_OPT"
+if test ! -z "$CONFIGURE_EXT_OPT"; then
+  echo "+ options passed to configure: $CONFIGURE_EXT_OPT"
 fi
 
 test -n "$NOCONFIGURE" && {
- echo 'Skipping configure stage';
- exit 0
+  echo "Skipping configure stage"
+  exit 0
 }
 
-$srcdir/configure $* || {
- echo 'configure failed';
- exit 1;
+$srcdir/configure $CONFIGURE_EXT_OPT || {
+  echo "configure failed"
+  exit 1
 }
 
 echo
